f823cc19c42c7f7ed7d5bad89d6c1fb5eb6966ac
never130 <never130@hotmail.com>
2023-03-18 00:55:58 -0300

    e3

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    /*En este ejercicio tenéis que crear un bucle que permita concatenar
+    textos e imprima el resultado final por consola.
+    */
+    public static void main(String[] args) {
+        String[] nombres = {"Ever ", "Cris ", "y ", "Manuela ", "Rocio"};
+
+        for (String nombre : nombres) {
+            System.out.print(nombre);
+        }
+    }
+}
